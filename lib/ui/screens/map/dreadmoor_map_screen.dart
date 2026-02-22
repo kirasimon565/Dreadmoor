2e7240404e31a167a36292adfa71b175e3b02b5d
kirasimon565 <kirasimon136@gmail.com>
2026-02-22 06:50:29 +0200

    Update dreadmoor_map_screen.dart

--- lib/ui/screens/map/dreadmoor_map_screen.dart
+++ lib/ui/screens/map/dreadmoor_map_screen.dart
@@ -1,107 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../core/state/map_state.dart';
 import '../../theme/colors.dart';
 import 'location_detail_sheet.dart';
 
-class DreadmoorMapScreen extends StatelessWidget {
+class DreadmoorMapScreen extends ConsumerWidget {
   const DreadmoorMapScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final unlockedAsync = ref.watch(unlockedLocationsProvider);
+
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          // Map Viewer
-          InteractiveViewer(
-            minScale: 0.5,
-            maxScale: 2.5,
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/map/dreadmore_map.png',
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                  errorBuilder: (c,e,s) => Container(color: Colors.grey[900], child: const Center(child: Text('Map Unavailable', style: TextStyle(color: Colors.white)))),
+      body: unlockedAsync.when(
+        data: (unlockedFlags) {
+          return Stack(
+            children: [
+              InteractiveViewer(
+                minScale: 0.6,
+                maxScale: 2.5,
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final w = constraints.maxWidth;
+                    final h = constraints.maxHeight;
+
+                    return Stack(
+                      children: [
+                        Image.asset(
+                          'assets/map/dreadmore_map.png',
+                          width: w,
+                          height: h,
+                          fit: BoxFit.cover,
+                        ),
+
+                        for (final loc in allMapLocations)
+                          _MapPin(
+                            location: loc,
+                            unlocked: unlockedFlags.contains(loc.requiredFlag),
+                            mapWidth: w,
+                            mapHeight: h,
+                          ),
+                      ],
+                    );
+                  },
                 ),
-
-                // Pins
-                _buildPin(context, 100, 200, 'factory'),
-                _buildPin(context, 300, 400, 'rebecca_home'),
-                _buildPin(context, 50, 150, 'restaurant'),
-                _buildPin(context, 250, 300, 'highway'),
-              ],
-            ),
-          ),
-
-          // Header Overlay
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 10, bottom: 10, left: 16, right: 16),
-              color: Colors.black.withOpacity(0.5),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back, color: Colors.white),
-                    onPressed: () => context.pop(),
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    "DREADMOOR",
-                    style: GoogleFonts.michroma(
-                      fontSize: 16,
-                      color: Colors.white,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ],
               ),
-            ),
-          ),
-        ],
+
+              _Header(onBack: () => context.pop()),
+            ],
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator(color: DreadmoorColors.accentCyan)),
+        error: (e, _) => Center(child: Text("MAP ERROR", style: GoogleFonts.michroma(color: Colors.red))),
       ),
     );
   }
+}
 
-  Widget _buildPin(BuildContext context, double x, double y, String locationId) {
+class _Header extends StatelessWidget {
+  final VoidCallback onBack;
+  const _Header({required this.onBack});
+
+  @override
+  Widget build(BuildContext context) {
     return Positioned(
-      left: x,
-      top: y,
-      child: GestureDetector(
-        onTap: () {
-          showModalBottomSheet(
-            context: context,
-            backgroundColor: Colors.transparent,
-            isScrollControlled: true,
-            builder: (context) => DraggableScrollableSheet(
-              initialChildSize: 0.4,
-              minChildSize: 0.2,
-              maxChildSize: 0.8,
-              builder: (context, scrollController) => SingleChildScrollView(
-                controller: scrollController,
-                child: LocationDetailSheet(locationId: locationId),
-              ),
-            ),
-          );
-        },
-        child: Column(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top + 10,
+          bottom: 10,
+          left: 16,
+          right: 16,
+        ),
+        color: Colors.black.withOpacity(0.55),
+        child: Row(
           children: [
-            const Icon(Icons.location_on, color: DreadmoorColors.accentRed, size: 32),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-              color: Colors.black.withOpacity(0.7),
-              child: Text(
-                locationId.toUpperCase().replaceAll('_', ' '),
-                style: GoogleFonts.michroma(fontSize: 10, color: Colors.white),
+            IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: onBack,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              "DREADMOOR",
+              style: GoogleFonts.michroma(
+                fontSize: 16,
+                color: Colors.white,
+                letterSpacing: 2.0,
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MapPin extends StatelessWidget {
+  final MapLocation location;
+  final bool unlocked;
+  final double mapWidth;
+  final double mapHeight;
+
+  const _MapPin({
+    required this.location,
+    required this.unlocked,
+    required this.mapWidth,
+    required this.mapHeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: mapWidth * location.x,
+      top: mapHeight * location.y,
+      child: GestureDetector(
+        onTap: unlocked
+            ? () {
+                showModalBottomSheet(
+                  context: context,
+                  backgroundColor: Colors.transparent,
+                  isScrollControlled: true,
+                  builder: (_) => LocationDetailSheet(location: location),
+                );
+              }
+            : null,
+        child: Opacity(
+          opacity: unlocked ? 1.0 : 0.3,
+          child: Column(
+            children: [
+              Icon(
+                Icons.location_on,
+                color: unlocked ? DreadmoorColors.accentRed : Colors.grey[700],
+                size: 32,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                color: Colors.black.withOpacity(0.7),
+                child: Text(
+                  location.title,
+                  style: GoogleFonts.michroma(fontSize: 9, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
