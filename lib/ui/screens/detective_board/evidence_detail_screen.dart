02a235233af78f378d2cf48169efae0b163f86b7
kirasimon565 <kirasimon136@gmail.com>
2026-02-23 04:28:27 +0200

    Update evidence_detail_screen.dart

--- lib/ui/screens/detective_board/evidence_detail_screen.dart
+++ lib/ui/screens/detective_board/evidence_detail_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:collection/collection.dart';
 
 import '../../../core/state/investigation_state.dart';
 import '../../theme/colors.dart';
