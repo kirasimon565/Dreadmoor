c0a62eb2c23433bff0c22da026b9cc1686cc96f9
kirasimon565 <kirasimon136@gmail.com>
2026-02-23 05:58:11 +0200

    Update messenger_list_screen.dart

--- lib/ui/screens/messenger_list/messenger_list_screen.dart
+++ lib/ui/screens/messenger_list/messenger_list_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
 
 import '../../../core/persistence/drift_database.dart';
 import '../../../core/state/game_state.dart';
