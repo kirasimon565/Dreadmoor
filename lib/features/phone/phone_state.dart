d75c19621f391a9e4c881f1e4424c29a98a31fc2
kirasimon565 <kirasimon136@gmail.com>
2026-03-08 21:02:37 +0200

    Update phone_state.dart

--- lib/features/phone/phone_state.dart
+++ lib/features/phone/phone_state.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dreadmoor/core/time/game_clock.dart';
-
 import 'package:dreadmoor/core/persistence/drift_database.dart';
 
 enum CallState { idle, incoming, active }
