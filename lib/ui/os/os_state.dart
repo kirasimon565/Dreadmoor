13c1dfd55466f0e7562d56051fa82aa7ccb91e83
kirasimon565 <kirasimon136@gmail.com>
2026-03-13 12:07:03 +0200

    Update os_state.dart

--- lib/ui/os/os_state.dart
+++ lib/ui/os/os_state.dart
@@ -10,6 +10,12 @@ enum PhoneApp {
   profile,
 }
 
+/// Controls which app is currently visible in the IndexedStack
 final activeAppProvider = StateProvider<PhoneApp>((ref) {
   return PhoneApp.messenger;
+});
+
+/// Controls the Glitch/Secret Intercept UI (Used in Scene 5)
+final isHackedProvider = StateProvider<bool>((ref) {
+  return false;
 });
