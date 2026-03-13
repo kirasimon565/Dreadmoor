2ae5caea7a7374f4ceb9966a5e903a37df9c5f25
kirasimon565 <kirasimon136@gmail.com>
2026-03-13 04:25:38 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
+            storeFile = rootProject.file("app/${keystoreProperties["storeFile"]}")
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
